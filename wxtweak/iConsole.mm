6f17f427fe9705d29f6a41a566fa1ef8fb9004d9
Jamy <suzhijie325@163.com>
2016-04-16 19:03:03 +0800

    new version

--- wxtweak/iConsole.mm
+++ wxtweak/iConsole.mm
@@ -38,7 +38,8 @@ static _Bool _logos_meta_method$_ungrouped$iConsole$shouldLog$(_LOGOS_SELF_TYPE_
 
 
 static void _logos_meta_method$_ungrouped$iConsole$logToFile$module$file$line$func$message$(_LOGOS_SELF_TYPE_NORMAL Class _LOGOS_SELF_CONST self, SEL _cmd, int arg1, const char * arg2, const char * arg3, int arg4, const char * arg5, id arg6)  {
-    HBLogDebug(@"+[<iConsole: %p> logToFile:%d module:0x%x file:0x%x line:%d func:0x%x message:%@]", self, arg1, (unsigned int)arg2, (unsigned int)arg3, arg4, (unsigned int)arg5, arg6);
+    
+    NSLog(@"%d-%s-%s-%d-%s-%@", arg1, arg2, arg3,arg4,arg5,arg6);
     _logos_meta_orig$_ungrouped$iConsole$logToFile$module$file$line$func$message$(self, _cmd, arg1, arg2, arg3, arg4, arg5, arg6);
    }
   
@@ -86,4 +87,4 @@ static void _logos_meta_method$_ungrouped$iConsole$logToFile$module$file$line$fu
 
 static __attribute__((constructor)) void _logosLocalInit() {
 {Class _logos_class$_ungrouped$iConsole = objc_getClass("iConsole"); Class _logos_metaclass$_ungrouped$iConsole = object_getClass(_logos_class$_ungrouped$iConsole); if (_logos_metaclass$_ungrouped$iConsole) {MSHookMessageEx(_logos_metaclass$_ungrouped$iConsole, @selector(shouldEnableDebugLog), (IMP)&_logos_meta_method$_ungrouped$iConsole$shouldEnableDebugLog, (IMP*)&_logos_meta_orig$_ungrouped$iConsole$shouldEnableDebugLog);} else {HBLogError(@"logos: nil class %s", "iConsole");}if (_logos_metaclass$_ungrouped$iConsole) {MSHookMessageEx(_logos_metaclass$_ungrouped$iConsole, @selector(purelog:), (IMP)&_logos_meta_method$_ungrouped$iConsole$purelog$, (IMP*)&_logos_meta_orig$_ungrouped$iConsole$purelog$);} else {HBLogError(@"logos: nil class %s", "iConsole");}if (_logos_metaclass$_ungrouped$iConsole) {MSHookMessageEx(_logos_metaclass$_ungrouped$iConsole, @selector(shouldLog:), (IMP)&_logos_meta_method$_ungrouped$iConsole$shouldLog$, (IMP*)&_logos_meta_orig$_ungrouped$iConsole$shouldLog$);} else {HBLogError(@"logos: nil class %s", "iConsole");}if (_logos_metaclass$_ungrouped$iConsole) {MSHookMessageEx(_logos_metaclass$_ungrouped$iConsole, @selector(logToFile:module:file:line:func:message:), (IMP)&_logos_meta_method$_ungrouped$iConsole$logToFile$module$file$line$func$message$, (IMP*)&_logos_meta_orig$_ungrouped$iConsole$logToFile$module$file$line$func$message$);} else {HBLogError(@"logos: nil class %s", "iConsole");}} }
-#line 62 "/Users/jamy/works/wxtweak/wxtweak/iConsole.xm"
+#line 63 "/Users/jamy/works/wxtweak/wxtweak/iConsole.xm"
